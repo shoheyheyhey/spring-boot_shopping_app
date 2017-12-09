73d583339cc4f7119178c454e7eb8a2d55804d00
shohei <shohei@192.168.179.3>
2017-12-10 00:26:20 +0900

    浅田：退会済みユーザはログインできないように修正

--- src/main/java/com/example/service/LoginService.java
+++ src/main/java/com/example/service/LoginService.java
@@ -1,10 +1,13 @@
 package com.example.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.entity.TblUser;
+import com.example.entity.TblUserExample;
 import com.example.mapper.TblUserMapper;
 
 @Service
@@ -19,11 +22,16 @@ public class LoginService {
 	 * @param TblUser
 	 * @return TblUser
 	 */
-	public TblUser login(TblUser user){		
-		TblUser authResult = tblUserMapper.selectByPrimaryKey(user.getUserId());
-		if (authResult != null && user.getPassword().equals(authResult.getPassword())){
+	public TblUser login(TblUser user){	
+		TblUserExample tblUserExample = new TblUserExample();
+		tblUserExample.createCriteria().andUserIdEqualTo(user.getUserId()).andDelFlgEqualTo("0");
+		List<TblUser> authResultList = tblUserMapper.selectByExample(tblUserExample);
+		if (authResultList != null && authResultList.size() > 0){
 			// ログイン成功
-			return authResult;
+			TblUser authResult = authResultList.get(0);
+			if(authResult.getPassword().equals(user.getPassword())) {
+				return authResult;
+			}
 		}
 		//ログイン失敗
 		return null;
